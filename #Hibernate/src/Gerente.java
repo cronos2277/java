39a95585b74bac3df89db81b99ad73ebd13ec5db
JP Windows <cronos2277@gmail.com>
2020-01-22 15:24:55 -0300

    classe gerente

---
+++ #Hibernate/src/Gerente.java
@@ -0,0 +1,94 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+public class Gerente {
+	//Essa constante contem a String que o session.createQuery vai executar
+	private final String consulta = "from Aluno";
+	private SessionFactory factory;
+	private Session session;
+	private List<Aluno> alunos;
+	public Gerente() {
+		sessionFactory();
+		this.alunos = new ArrayList<Aluno>();
+	}
+	
+	private void sessionFactory() {
+		try {
+			this.factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		}catch(Exception e) {			
+			System.err.println("Lançado na função sessionFactory");
+			formatError(e);
+		}
+	}
+	
+	private void getSession() {
+		try {
+			if(this.factory.isClosed()) {
+				this.sessionFactory();
+			}
+			this.session = factory.openSession();
+		}catch(Exception e) {
+			System.err.println("Lançado na função getSession");
+			formatError(e);
+		}
+	}
+	
+	private void closeSession() {
+		try {
+			this.session.close();
+			this.factory.close();
+		}catch(Exception e) {
+			System.err.println("Lançado na função closeSession");
+			formatError(e);
+		}
+	}
+	
+	/* 	
+	  Aqui retorna a lista de alunos, se a lista de alunos estiver vazia, ou se o booleano
+		isUpdate estiver verdadeiro, ele acessa o banco e pega os dados atualizados, do contrario
+		é pego da lista salva, isso evita um excesso de requisição ao banco de dados. 
+	*/
+	public List<Aluno> getAlunos(boolean isUpdate){
+		try {
+			if(this.alunos.isEmpty() || isUpdate) {
+				this.getSession();
+				this.alunos = session.createQuery(consulta).list();
+				this.closeSession();
+				return this.alunos;
+			}else {
+				return this.alunos;
+			}
+		}catch(Exception e) {
+			System.err.println("Lançado na função getAlunos");
+			formatError(e);
+			return null;
+		}
+	}
+	
+	public Aluno getAluno(int id) {
+		return this.alunos.get(id);
+	}
+	
+	public Aluno getAluno(int id, boolean forceUpdate) {		
+		if(forceUpdate) {
+			this.getAlunos(forceUpdate);
+			return this.alunos.get(id);
+		}else {
+			return this.alunos.get(id);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Existem: "+this.alunos.size()+", alunos registrados!";
+	}
+	
+	private void formatError(Exception exception) {
+		System.out.println(exception);
+		JOptionPane.showMessageDialog(null, exception);
+	}
+	
+}
---
+++ #Hibernate/src/ListarAlunos2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+public class ListarAlunos2 {
+	//Hibernate: update alunos set alu_nome=?, alu_cidade=?, alu_fone=? where alu_codigo=?
+    public static void main(String args[]){
+        Gerente gerente = new Gerente();
+        System.out.println(gerente);
+        gerente.getAlunos(true);
+        System.out.println(gerente);
+    }
+}
