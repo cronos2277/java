3aef49bafdcef2b861444ef0347438aec474ecf2
JP Windows <cronos2277@gmail.com>
2020-10-04 18:48:25 -0300

    Main Classe persistencia

--- Aulas/Spring/persistencia/Main.java
+++ Aulas/Spring/persistencia/Main.java
@@ -1,5 +1,8 @@
 package Spring.persistence;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,7 +12,14 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext app = new ClassPathXmlApplicationContext("/Spring/persistence/persistence.xml");
 		JdbcTemplate jdbc = (JdbcTemplate) app.getBean("dao");	
-		System.out.println(jdbc);
+		jdbc.execute("create table if not exists valor (id serial primary key,numero decimal(10,2) not null)");
+
+		int rowAffected = jdbc.update("insert into valor(numero) values (?)",Math.random());
+		System.out.println(rowAffected);
+		
+		List<Map<String,Object>> listas = jdbc.queryForList("Select * from valor");
+		System.out.println(listas);
+		
 
 	}
 
